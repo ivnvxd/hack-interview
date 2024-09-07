134281ddfee359c2fd0e6d1f954ead6a3cd53086
Andrey Ivanov <ivnv.xd@gmail.com>
2024-09-07 20:38:54 +0100

    feat: add gpt-4o and gpt-4o-mini models

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ THEME = "DarkGray12"
 OUTPUT_FILE_NAME = "record.wav"
 SAMPLE_RATE = 48000
 
-MODELS = ["gpt-3.5-turbo", "gpt-4-turbo-preview"]
+MODELS = ["gpt-4o-mini", "gpt-4o", "gpt-4-turbo", "gpt-3.5-turbo"]
 DEFAULT_MODEL = MODELS[0]
 
 DEFAULT_POSITION = "Python Developer"
